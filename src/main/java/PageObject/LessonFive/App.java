6feb5b747ba69ee35bced6057f2f849313e3b405
11486 <11486@LAPTOP-UKNL6QE2>
2018-06-03 12:07:14 +0800

    LessonFive Demonstration

---
+++ src/main/java/PageObject/LessonFive/App.java
@@ -0,0 +1,13 @@
+package PageObject.LessonFive;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/PageObject/LessonFive/HelloWorld.java
@@ -0,0 +1,10 @@
+package PageObject.LessonFive;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+	@Test
+	public void helloworld() {
+		System.out.println("Helloworld");
+	}
+}
